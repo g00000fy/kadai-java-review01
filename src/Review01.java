31ec580c7e23d5edc12002ada8f224713b17a825
GitHubへ登録したユーザ名 <GitHub>
2023-11-20 21:57:53 +0900

    Lesson3 課題提出

--- src/Review01.java
+++ src/Review01.java
@@ -6,10 +6,10 @@ public class Review01 {
 		int price = 1500;
 		double taxRate = 0.1;
 		int result; //税額
-		result = taxMethod(price, taxRate);
-		System.out.println(price + "円の商品の税込価格は" + (int)(price + (price * taxRate)) + "円（消費税は" + result + "円）です。");
+		result = tax(price, taxRate);
+		System.out.println(price + "円の商品の税込価格は" + (price + result) + "円（消費税は" + result + "円）です。");
 	}
-	public static int taxMethod(int price, double taxRate) {
+	public static int tax(int price, double taxRate) {
 		int result = (int)(price * taxRate);
 		return result;
 
